b89cdc75ca5acf88bef04dff16cc57e6cd56f265
fadila <anemfadilath@gmail.com>
2023-01-17 15:14:42 +0100

    premier commit

---
+++ max.java
@@ -0,0 +1,8 @@
+public class maxmin {
+	public class MaxAndMinValues{
+	public static void main(String[] args) {
+		System.out.print("la plus grande valeur in est "+Integer.MAX_VALUE + "la plus grande valeur flot est"+Float.MAX_VALUE);
+		
+	}
+	}
+}
